bc09cde72368edc570fd296badb6050b1b831114
John Armstrong <j.armstrong484@gmail.com>
2016-11-19 12:25:33 +1300

    Added the material-ui library for styling

--- app/app.jsx
+++ app/app.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App.component.jsx';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+import injectTapEventPlugin from 'react-tap-event-plugin';
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+class Main extends React.Component {
+    render() {
+        return (
+            <MuiThemeProvider>
+                <App></App>
+            </MuiThemeProvider>
+        )
+    }
+}
+ReactDOM.render(<Main />, document.getElementById('app'));
