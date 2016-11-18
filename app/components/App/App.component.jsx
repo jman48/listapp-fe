70a220dad4006e2d5e395aedbde9c0efbd5a319d
John Armstrong <j.armstrong484@gmail.com>
2016-11-19 09:56:40 +1300

    Added App component, which is the parent component of the application

---
+++ app/components/App/App.component.jsx
@@ -0,0 +1,14 @@
+import styles from './_App.style.scss';
+
+import React from 'react';
+
+export default class App extends React.Component {
+
+    render() {
+        return (
+            <div className="app">
+            </div>
+        );
+    }
+
+}
