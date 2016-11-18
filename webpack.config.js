cf89f15c0fe59df4752a78db556bd46ec9027bf9
John Armstrong <j.armstrong484@gmail.com>
2016-11-19 09:33:36 +1300

    Added projects entry files and wired them for webpack to build and serve

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './app.component.js',
+    output: { path: __dirname, filename: 'bundle.js' },
+    module: {
+        loaders: [
+            {
+                test: /\.scss$/,
+                loaders: ['style', 'css', 'sass']
+            },
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    },
+};
