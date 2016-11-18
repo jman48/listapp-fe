947f5dafde199b5e8f1a1020c6e7593f555ce8b9
John Armstrong <j.armstrong484@gmail.com>
2016-11-19 11:45:17 +1300

    Lists component now gets it state from the lists store

--- app/components/App/App.component.jsx
+++ app/components/App/App.component.jsx
@@ -5,19 +5,11 @@ import Lists from '../Lists/Lists.component.jsx';
 
 export default class App extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.props = props;
-
-        //Used just for development
-        this.lists = [{name: "test list 1"}];
-    }
-
     render() {
         return (
             <div className="app">
                 <h1>Hello !</h1>
-                <Lists lists={this.lists}></Lists>
+                <Lists></Lists>
             </div>
         );
     }
--- app/components/Lists/Lists.component.jsx
+++ app/components/Lists/Lists.component.jsx
@@ -1,12 +1,36 @@
 import style from './_Lists.style.scss';
 
 import React from 'react';
+import ListStore from './Lists.store.js';
+import ListActions from './Lists.actions';
 
 export default class Lists extends React.Component {
+    constructor() {
+        super(...arguments);
+        this.state = {
+            lists: ListStore.getState()
+        };
+    }
+
+    componentDidMount() {
+        this.storeSubscription = ListStore.addListener(() => {
+            this.handleChange();
+        });
+
+        ListActions.loadLists();
+    }
+
+    componentWillUnmount() {
+        this.storeSubscription.remove();
+    }
+
+    handleChange() {
+        this.setState({lists: ListStore.getState()});
+    }
 
     render() {
-        let renderedLists = this.props.lists.map((list) => {
-            return <li>{list.name}</li>
+        let renderedLists = this.state.lists.map((list) => {
+            return <li key={list.name}>{list.name}</li>
         });
 
         return (
