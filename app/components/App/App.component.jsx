61a1ad91a30106d7712f89c4b3dba473f206ee00
John Armstrong <j.armstrong484@gmail.com>
2016-11-19 14:00:20 +1300

    Added react router to application

--- app/components/App/App.component.jsx
+++ app/components/App/App.component.jsx
@@ -2,14 +2,17 @@ import styles from './_App.style.scss';
 
 import React from 'react';
 import Lists from '../Lists/Lists.component.jsx';
+import {Router, Route, hashHistory} from 'react-router'
 
 export default class App extends React.Component {
 
     render() {
         return (
             <div className="app">
-                <h1>Hello !</h1>
-                <Lists></Lists>
+                <h1>ListApp</h1>
+                <Router history={hashHistory}>
+                    <Route path="/" component={Lists}/>
+                </Router>
             </div>
         );
     }
