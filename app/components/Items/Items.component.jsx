f6381259f3781f5c5ddbfb59077aad34028497d7
John Armstrong <j.armstrong484@gmail.com>
2016-11-19 18:26:47 +1300

    Added an item component to deal with all items of a list

---
+++ app/components/Items/Items.component.jsx
@@ -0,0 +1,12 @@
+import style from './_Items.style.scss';
+
+import React from 'react';
+
+export default class Items extends React.Component {
+
+    render() {
+        return (
+            <h1>Items</h1>
+        )
+    }
+}
