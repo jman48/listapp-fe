13965ea617093f572f21ee56af5568f4e23fc3a2
John Armstrong <j.armstrong484@gmail.com>
2016-11-20 11:59:32 +1300

    Items component will not load the whole list and display each item of the list

--- app/components/Items/Items.component.jsx
+++ app/components/Items/Items.component.jsx
@@ -2,11 +2,48 @@ import style from './_Items.style.scss';
 
 import React from 'react';
 
+import ItemStore from '../../stores/Items.store.js';
+import ItemActions from '../../actions/Items.actions';
+
+import {List, ListItem} from 'material-ui/List';
+
 export default class Items extends React.Component {
 
+    constructor() {
+        super(...arguments);
+        this.state = {
+            itemList: ItemStore.getState()
+        }
+    }
+
+    componentDidMount() {
+        this.storeSubscription = ItemStore.addListener(() => {
+            this.update();
+        });
+
+        ItemActions.loadList(this.props.params.id);
+    }
+
+    componentWillUnmount() {
+        this.storeSubscription.remove();
+    }
+
+    update() {
+        this.setState({itemList: ItemStore.getState()});
+    }
+
     render() {
+        let items = this.state.itemList.items.map((item) => {
+            return <ListItem key={item.name} primaryText={item.name}></ListItem>
+        });
+
         return (
-            <h1>Items for {this.props.params.id}</h1>
+            <div className="items">
+                <h1>Items for {this.state.itemList.name}</h1>
+                <List>
+                    {items}
+                </List>
+            </div>
         )
     }
 }
