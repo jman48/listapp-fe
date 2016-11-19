152d8956541917ca709dcad48e53272b14c95638
John Armstrong <j.armstrong484@gmail.com>
2016-11-19 13:49:02 +1300

    Wired up the delete list functionality to the list component so a user can delete a list

--- app/components/List/List.component.jsx
+++ app/components/List/List.component.jsx
@@ -1,15 +1,27 @@
 import style from './_List.style.scss';
 
 import React, {PropTypes} from 'react';
+import IconButton from 'material-ui/IconButton';
+import ContentClear from 'material-ui/svg-icons/content/clear';
+
+import ListActions from '../Lists/Lists.actions';
 
 /**
  * This component will display a single list
  */
 export default class List extends React.Component {
+
+    deleteList() {
+        ListActions.deleteList(this.props.list);
+    }
+
     render() {
         return (
             <li className="list">
                 {this.props.list.name}
+                <IconButton onClick={this.deleteList.bind(this)}>
+                    <ContentClear />
+                </IconButton>
             </li>
         )
     }
