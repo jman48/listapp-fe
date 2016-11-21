533116b35a44484d0b3e29f5dd3954f1b1afc274
John Armstrong <j.armstrong484@gmail.com>
2016-11-21 18:10:17 +1300

    Added addDialog component to items component so that we can add new items

--- app/components/Items/Items.component.jsx
+++ app/components/Items/Items.component.jsx
@@ -5,15 +5,19 @@ import React from 'react';
 import ItemStore from '../../stores/Items.store.js';
 import ItemActions from '../../actions/Items.actions';
 import Item from '../Item/Item.component.jsx';
+import AddDialog from '../AddDialog/AddDialog.component.jsx';
 
 import {List} from 'material-ui/List';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
 
 export default class Items extends React.Component {
 
     constructor() {
         super(...arguments);
         this.state = {
-            itemList: ItemStore.getState()
+            itemList: ItemStore.getState(),
+            addDialogOpen: false
         }
     }
 
@@ -29,13 +33,26 @@ export default class Items extends React.Component {
         this.storeSubscription.remove();
     }
 
+    openDialog() {
+        this.setState({addDialogOpen: true});
+    }
+
+    closeDialog() {
+        this.setState({addDialogOpen: false});
+    }
+
     update() {
         this.setState({itemList: ItemStore.getState()});
     }
 
+    onAdd(newItem) {
+        ItemActions.addItem(this.state.itemList._id, newItem);
+        this.closeDialog();
+    }
+
     render() {
         let items = this.state.itemList.items.map((item) => {
-            return <Item key={item._id} item={item} list={this.state.itemList._id}></Item>
+            return <Item key={item._id} item={item} list={this.state.itemList._id}/>
         });
 
         return (
@@ -44,6 +61,18 @@ export default class Items extends React.Component {
                 <List>
                     {items}
                 </List>
+
+                <AddDialog
+                    open={this.state.addDialogOpen}
+                    title="Add New Item"
+                    onAdd={this.onAdd.bind(this)}
+                    onClose={this.closeDialog.bind(this)}/>
+
+                <FloatingActionButton
+                    className="add-button"
+                    onClick={this.openDialog.bind(this)}>
+                    <ContentAdd />
+                </FloatingActionButton>
             </div>
         )
     }
